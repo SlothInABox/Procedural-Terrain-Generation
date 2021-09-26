0fc7d65b5326acc7429fe1239628ad5ff7f8e71a
James Goodman <43926484+SlothInABox@users.noreply.github.com>
2021-09-26 12:52:38 +0100

    Added noise generator
    - Noise texture created with perlin noise.
    - Noise rendered as a texture in editor.

---
+++ Assets/Editor/MapGeneratorEditor.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor (typeof(MapGenerator))]
+public class MapGeneratorEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        MapGenerator mapGen = (MapGenerator)target;
+
+        DrawDefaultInspector();
+
+        if (DrawDefaultInspector())
+        {
+            if (mapGen.autoUpdate)
+            {
+                mapGen.GenerateMap();
+            }
+        }
+
+        if (GUILayout.Button("Generate"))
+        {
+            mapGen.GenerateMap();
+        }
+    }
+}
--- Assets/Scripts/Noise.cs
+++ Assets/Scripts/Noise.cs
@@ -2,17 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Noise : MonoBehaviour
+public static class Noise
 {
-    // Start is called before the first frame update
-    void Start()
+    public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, float scale)
     {
-        
-    }
+        float[,] noiseMap = new float[mapWidth, mapHeight];
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (scale <= 0)
+        {
+            scale = 0.0001f;
+        }
+
+        for (int y = 0; y < mapHeight; y++)
+        {
+            for (int x = 0; x < mapWidth; x++)
+            {
+                float sampleX = x / scale;
+                float sampleY = y / scale;
+
+                float perlinValue = Mathf.PerlinNoise(sampleX, sampleY);
+                noiseMap[x, y] = perlinValue;
+            }
+        }
+
+        return noiseMap;
     }
 }
